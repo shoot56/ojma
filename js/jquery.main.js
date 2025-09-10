91f7eeb0695c3069cc27e611f2587856cbfc1ec4
Dmytro Shutko <shoot56@gmail.com>
2025-09-10 12:17:09 +0300

    v1.0.3 add mobile

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,6 +1,22 @@
 (function($){
 	$(document).ready(function () {
 		initVideoBlocks();
+		// scroll to
+		$('a[href*=\\#]:not([href=\\#])').on('click', function() {
+			let target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - 72
+				}, 800);
+				return false;
+			}
+		});
+		// mobile menu
+		$(".nav-opener").click(function(e) {
+			e.preventDefault();
+			$('body').toggleClass('nav-active');
+		});
 	});
 })(jQuery);
 
